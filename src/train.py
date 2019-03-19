ebe4ed5e84582cced924138b6aca3517ccba89a1
oh-sore-takesou <oh.sore.sore.soutarou@gmail.com>
2019-03-19 19:49:51 +0900

    Added some comments

--- src/train.py
+++ src/train.py
@@ -19,12 +19,15 @@ def run(datadir, savedir, context_size=4, bsize=32, hid_n=300, lr=0.001,
     datadir = Path(datadir)
     savedir = Path(savedir)
 
+    # Tensorboard logger
     if use_tb:
         tb = SummaryWriter(str(savedir))
 
+    # Load dataset (Already tokenized)
     t2i, words = pickle.load(open(datadir / 'vocab.pkl', 'rb'))
     dataset = pickle.load(open(datadir / 'dataset.token.pkl', 'rb'))
 
+    # Prepare pytorch dataloader
     dataloader = get_loaders(dataset, context_size, t2i[PAD_TOKEN], bsize)
 
     voc_n = len(t2i)
@@ -36,22 +39,21 @@ def run(datadir, savedir, context_size=4, bsize=32, hid_n=300, lr=0.001,
     print('Doc n:', doc_n)
     print('Device: ', device)
 
+    # Prepare Model
     model = PVDM(voc_n, doc_n, hid_n)
     model = model.to(device)
     optimizer = optim.SGD(model.parameters(), lr=lr)
 
+    # Loss function
     loss_func = nn.CrossEntropyLoss()
     best_loss = 1e+5
 
     for i_epoch in range(1, epoch + 1):
         losses = []
         for doc_ids, contexts, targets in dataloader:
-            # doc_ids -> [B]
-            # contexts -> [B, context_size]
-            # targets -> [B]
-            doc_ids = doc_ids.to(device)
-            contexts = contexts.to(device)
-            targets = targets.to(device)
+            doc_ids = doc_ids.to(device)  # [B]
+            contexts = contexts.to(device)  # [B, context_size]
+            targets = targets.to(device)  # [B]
 
             optimizer.zero_grad()
             y = model(doc_ids, contexts)  # [B, V]
@@ -60,14 +62,17 @@ def run(datadir, savedir, context_size=4, bsize=32, hid_n=300, lr=0.001,
             optimizer.step()
 
             losses.append(loss.item())
+
         mean_loss = np.mean(losses)
         print('Loss: ', mean_loss)
 
+        # Tensorboard logging
         if use_tb:
             tb.add_scalar(
                     'train/loss', mean_loss, i_epoch
                     )
 
+        # Make checkpoints
         if mean_loss < best_loss:
             best_loss = mean_loss
             # Dump model
